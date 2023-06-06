a16c49d067fc5b5b1d6e901a2e030137e7aefb17
Heila Al-Mogren <halmgren@tahakom.com>
2023-06-06 11:52:04 +0300

    Created Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import {Button, ButtonGroup} from '@chakra-ui/react'
+import {Grid, GridItem, Show} from '@chakra-ui/react'
 import './App.css'
 
 
 function App() {
 
 
-    return (<></>)
+    return (<Grid templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`
+    }
+    }>
+        <GridItem area="nav">
+NavBar
+        </GridItem>
+        <Show above="lg">
+            <GridItem area="aside" bg="gold">
+                Aside
+            </GridItem>
+        </Show>
+
+        <GridItem area="main" bg="dodgerblue">
+            Main
+        </GridItem>
+    </Grid>)
 
 }
 
